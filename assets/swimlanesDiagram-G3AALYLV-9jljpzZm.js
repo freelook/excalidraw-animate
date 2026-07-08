b1af8e83fd96cdf1d11dceedcec15abf3fc97e20
freelook <7212860+freelook@users.noreply.github.com>
2026-07-08 15:01:21 +0200

    Updates

---
+++ assets/swimlanesDiagram-G3AALYLV-9jljpzZm.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-C9Dmm5vL.js";import{_ as a}from"./index-Dkx0Lpem.js";import"./chunk-5VM5RSS4-DJ1sjrZj.js";import"./chunk-XXDRQBXY-Z2-Xi9Fg.js";import"./chunk-VR4S4FIN-BjOaV2Ze.js";import"./chunk-32BRIVSS-DPZGBofT.js";import"./channel-D8v9jnlb.js";import"./index-f2YwMBZu.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
